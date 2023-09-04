77fec97684eec7e0e0e0fefd4f4e8aed5a216a4d
hernandomv92 <hernandomv@gmai.com>
2023-09-04 12:56:50 -0500

    segundo commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://hernandomv92.github.io',
+	base: '/BlogPersonal',
 	integrations: [mdx(), sitemap()],
 });
