14f0efaef1c8c5c74b018593ebe6ab9f20cf1726
hernandomv92 <hernandomv@gmai.com>
2023-09-07 05:10:05 -0500

    Index, About

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'NandoBlog';
+export const SITE_TITLE = 'Nando Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
